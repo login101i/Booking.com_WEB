298da089f476dbbb0af17aacd42dd963ffa99546
login101i <maciejkruszyniak@gmail.com>
2022-07-09 18:21:27 +0200

    hotel CRUD operations, error handling util

--- api/index.js
+++ api/index.js
@@ -14,41 +14,43 @@ dotenv.config();
 
 
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to mongoDB".magenta);
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!".red);
+});
+
+//middlewares
+app.use(express.json());
+
+// routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-const connect = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		});
-		console.log("Connected to mongoDB".magenta);
-	} catch (error) {
-		throw error;
-	}
-};
-
-mongoose.connection.on("disconnected", () => {
-	console.log("mongoDB disconnected!".red);
-});
-
-//middlewares
-
 app.use((err, req, res, next) => {
-	const errorStatus = err.status || 500;
-	const errorMessage = err.message || "Something went wrong!".red;
-	return res.status(errorStatus).json({
-		success: false,
-		status: errorStatus,
-		message: errorMessage,
-		stack: err.stack
-	});
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!".red;
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack
+  });
 });
 
 app.listen(8800, () => {
-	connect();
-	console.log("Connected to backend.".blue);
+  connect();
+  console.log("Connected to backend.".blue);
 });
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,8 +1,22 @@
-import express from 'express'
+import express from "express";
+import { createHotel, updateHotel, getHotel, getHotels, deleteHotel } from "../controllers/hotel.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", createHotel);
+
+//UPDATE
+router.put("/:id", updateHotel);
+
+// GET  
+router.get("/find/:id", getHotel)
+
+// GET ALL
+router.get('/', getHotels)
+
+// REMOVE HOTEL
+router.delete('/:id', deleteHotel)
 
 
-const router=express.Router()
-
-
-
-export default router
+export default router;
