855ae1d9aa4ea17ad70308f1ad1666be07f90259
mkruszyniak <maciejkruszyniak@gmail.com>
2022-12-09 15:58:06 +0100

    config update

--- api/index.js
+++ api/index.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
 	app.use(express.static(path.join(__dirname, '/client/build/index.html')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frotnend', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 	});
 } else {
 	app.get('/', (req, res) => {
